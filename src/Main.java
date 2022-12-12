c49e67363c717e8bf9ca0b06ef862621adde7b2e
b1lodHand <usturakeserway@gmail.com>
2022-12-12 20:56:03 +0300

    cleaned the root main class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-import com.librarymanagement.Book;
-import com.librarymanagement.BookType;
-import com.librarymanagement.LibraryManagementSystem;
-import com.librarymanagement.RentInfo;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
